141e4fcab8ebf93046fe82622ed43b0ff19fac1a
Kantslo <gqancliani@gmail.com>
2024-03-01 21:41:04 +0400

    feat: added cart item model using sequelize

---
+++ models/cart-item.js
@@ -0,0 +1,17 @@
+const Sequelize = require("sequelize");
+
+const sequelize = require("../util/database.js");
+
+const CartItem = sequelize.define("cartItem", {
+  id: {
+    type: Sequelize.STRING,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  quantity: {
+    type: Sequelize.INTEGER,
+  },
+});
+
+module.exports = CartItem;
