1719dfc701bf4bdb1c0bfb41e9059d1a8fc8f8da
Kantslo <gqancliani@gmail.com>
2024-03-01 21:49:19 +0400

    fix: fixed an id type to integer in cart and cart-item models

--- models/cart-item.js
+++ models/cart-item.js
@@ -4,7 +4,7 @@ const sequelize = require("../util/database.js");
 
 const CartItem = sequelize.define("cartItem", {
   id: {
-    type: Sequelize.STRING,
+    type: Sequelize.INTEGER,
     autoIncrement: true,
     allowNull: false,
     primaryKey: true,
