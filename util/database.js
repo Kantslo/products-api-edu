99ec8e7476260c5d252c44d1a5e456987ff8c905
Kantslo <gqancliani@gmail.com>
2024-03-03 02:42:01 +0400

    chore: replaced database information with env varibales

--- util/database.js
+++ util/database.js
@@ -1,8 +1,13 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("products-edu", "root", "@Kantslo113", {
-  dialect: "mysql",
-  host: "localhost",
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    dialect: process.env.DB_DIALECT,
+    host: process.env.DB_HOST,
+  }
+);
 
 module.exports = sequelize;
