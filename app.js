9180b616499e66e9c93a48548ed4ba2872a83c49
Kantslo <gqancliani@gmail.com>
2024-03-15 21:18:48 +0400

    added user creation with mongo db instead of sql

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
   User.findById("65ef6e72c888da0233c4c0f4")
     .then((user) => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch((err) => {
