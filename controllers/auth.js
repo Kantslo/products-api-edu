00da49026addbc95aed19c248a9c9eb5316e6a53
Kantslo <gqancliani@gmail.com>
2024-03-20 00:03:51 +0400

    feat: added signup controller functions to auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,21 +1,39 @@
-const User = require("../models/user.js");
+const User = require("../models/user");
 
 exports.getLogin = (req, res, next) => {
   res.render("auth/login", {
     path: "/login",
-    pageTitle: "Your Orders",
-    isAuthenticated: req.session.isLoggedIn,
+    pageTitle: "Login",
+    isAuthenticated: false,
+  });
+};
+
+exports.getSignup = (req, res, next) => {
+  res.render("auth/signup", {
+    path: "/signup",
+    pageTitle: "Signup",
+    isAuthenticated: false,
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById("65f4f779ed6d26cfab4633a1")
+  User.findById("5bab316ce0a7c75f783cb8a8")
     .then((user) => {
-      req.session.user = user;
       req.session.isLoggedIn = true;
-      res.redirect("/");
+      req.session.user = user;
+      req.session.save((err) => {
+        console.log(err);
+        res.redirect("/");
+      });
     })
-    .catch((err) => {
-      console.log(err);
-    });
+    .catch((err) => console.log(err));
+};
+
+exports.postSignup = (req, res, next) => {};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy((err) => {
+    console.log(err);
+    res.redirect("/");
+  });
 };
