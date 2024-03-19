d39babf97a45654c298936d3b8a325978fd784e3
Kantslo <gqancliani@gmail.com>
2024-03-19 19:44:10 +0400

    feat: registered auth route file

--- app.js
+++ app.js
@@ -15,8 +15,9 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+const adminRoutes = require("./routes/admin.js");
+const shopRoutes = require("./routes/shop.js");
+const authRoutes = require("./routes/auth.js");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -33,6 +34,7 @@ app.use((req, res, next) => {
 });
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
