62d6ece885a1ac5e9734f710bfc360a1773ac770
Kantslo <gqancliani@gmail.com>
2024-03-25 20:17:19 +0400

    feat: added csrf token protection

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
+const csrf = require("csurf");
 
 const errorController = require("./controllers/error.js");
 const User = require("./models/user.js");
@@ -17,6 +18,7 @@ const store = new MongoDBStore({
   uri: process.env.MONGO_URL,
   collection: "sessions",
 });
+const csrfProtection = csrf({});
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -35,6 +37,7 @@ app.use(
     store: store,
   })
 );
+app.use(csrfProtection);
 
 app.use((req, res, next) => {
   if (!req.session.user) {
@@ -48,6 +51,12 @@ app.use((req, res, next) => {
     .catch((err) => {
       console.log(err);
     });
+});
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
 });
 
 app.use("/admin", adminRoutes);
