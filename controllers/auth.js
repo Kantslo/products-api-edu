80bf420e6891eeb19859954dd898c80d6b47dc92
Kantslo <gqancliani@gmail.com>
2024-03-26 19:01:38 +0400

    feat: added nodemailer transport on signup

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,15 @@
 const bcrypt = require("bcrypt");
+const nodemailer = require("nodemailer");
 
 const User = require("../models/user");
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.GMAIL_USER,
+    pass: process.env.GMAIL_PASS,
+  },
+});
 
 exports.getLogin = (req, res, next) => {
   let message = req.flash("error");
@@ -84,6 +93,15 @@ exports.postSignup = (req, res, next) => {
         })
         .then((result) => {
           res.redirect("/login");
+          return transporter.sendMail({
+            to: email,
+            from: process.env.GMAIL_USER,
+            subject: "Signup succeeded",
+            html: "<h1>You successfully signed up!</h1>",
+          });
+        })
+        .catch((err) => {
+          console.log(err);
         });
     })
     .catch((err) => {
