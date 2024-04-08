26b6f6c6c013b3c92e7864b5c75b164e8ae31b04
Kantslo <gqancliani@gmail.com>
2024-04-08 21:51:37 +0400

    feat: added errors from express validator

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,3 +1,5 @@
+const { validationResult } = require("express-validator");
+
 const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
@@ -5,6 +7,8 @@ exports.getAddProduct = (req, res, next) => {
         pageTitle: "Add Product",
         path: "/admin/add-product",
         editing: false,
+        hasError: false,
+        errorMessage: null,
     });
 };
 
@@ -13,6 +17,23 @@ exports.postAddProduct = (req, res, next) => {
     const imageUrl = req.body.imageUrl;
     const price = req.body.price;
     const description = req.body.description;
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(422).render("admin/edit-product", {
+            pageTitle: "Add Product",
+            path: "/admin/edit-product",
+            editing: false,
+            hasError: true,
+            product: {
+                title: title,
+                imageUrl: imageUrl,
+                price: price,
+                description: description,
+            },
+            errorMessage: errors.array()[0].msg,
+        });
+    }
 
     const product = new Product({
         title: title,
@@ -39,7 +60,6 @@ exports.getEditProduct = (req, res, next) => {
     }
     const prodId = req.params.productId;
     Product.findById(prodId)
-        // Product.findById(prodId)
         .then((product) => {
             if (!product) {
                 return res.redirect("/");
@@ -49,6 +69,8 @@ exports.getEditProduct = (req, res, next) => {
                 path: "/admin/edit-product",
                 editing: editMode,
                 product: product,
+                hasError: false,
+                errorMessage: null,
             });
         })
         .catch((err) => console.log(err));
