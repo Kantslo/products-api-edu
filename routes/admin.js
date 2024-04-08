3c98a052037dbf1342d5784f9e522ed4a05455ab
Kantslo <gqancliani@gmail.com>
2024-04-09 00:30:21 +0400

    feat: added express validator error handling

--- routes/admin.js
+++ routes/admin.js
@@ -17,7 +17,7 @@ router.post(
     "/add-product",
     isAuth,
     [
-        body("title").isAlphanumeric().isLength({ min: 3 }).trim(),
+        body("title").isString().isLength({ min: 3 }).trim(),
         body("imageUrl").isURL(),
         body("price").isFloat(),
         body("description").isLength({ min: 5, max: 400 }).trim(),
