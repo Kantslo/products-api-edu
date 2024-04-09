e57ae6df46403a148c1d4640173b201c48d1d7a0
Kantslo <gqancliani@gmail.com>
2024-04-09 04:05:55 +0400

    feat: added error middlewares to main file

--- app.js
+++ app.js
@@ -47,11 +47,14 @@ app.use((req, res, next) => {
     }
     User.findById(req.session.user._id)
         .then((user) => {
+            if (!user) {
+                return next();
+            }
             req.user = user;
             next();
         })
         .catch((err) => {
-            console.log(err);
+            throw new Error(err);
         });
 });
 
@@ -65,7 +68,13 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
+app.use("/500", errorController.get500);
+
 app.use(errorController.get404);
+
+app.use((error, req, res, next) => {
+    res.redirect("/500");
+});
 
 mongoose
     .connect(process.env.MONGO_URI)
