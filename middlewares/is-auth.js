854866f2beab86adc7943b6206604c9ec7670f73
Kantslo <gqancliani@gmail.com>
2024-04-17 17:48:12 +0400

    chore: format code

--- middlewares/is-auth.js
+++ middlewares/is-auth.js
@@ -1,6 +1,7 @@
 module.exports = (req, res, next) => {
-  if (!req.session.isLoggedIn) {
-    return res.redirect("/login");
-  }
-  next();
+    if (!req.session.isLoggedIn) {
+        console.log(req.session.isLoggedIn, "AUTHENTICATION");
+        return res.redirect("/login");
+    }
+    next();
 };
