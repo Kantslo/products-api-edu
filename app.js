368de3144f8ba10320544936498e677c53c3771b
Kantslo <gqancliani@gmail.com>
2024-04-19 17:42:41 +0400

    chore: format document

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ dotenv.config();
 
 const app = express();
 const store = new MongoDBStore({
-    uri: process.env.MONGO_URI,
-    collection: "sessions",
+  uri: process.env.MONGO_URI,
+  collection: "sessions",
 });
 const csrfProtection = csrf({});
 
@@ -31,37 +31,37 @@ const authRoutes = require("./routes/auth.js");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
-    session({
-        secret: process.env.SESSION_SECRET_KEY,
-        resave: false,
-        saveUninitialized: false,
-        store: store,
-    })
+  session({
+    secret: process.env.SESSION_SECRET_KEY,
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
 );
 app.use(csrfProtection);
 app.use(flash());
 
 app.use((req, res, next) => {
-    res.locals.isAuthenticated = req.session.isLoggedIn;
-    res.locals.csrfToken = req.csrfToken();
-    next();
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
 });
 
 app.use((req, res, next) => {
-    if (!req.session.user) {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then((user) => {
+      if (!user) {
         return next();
-    }
-    User.findById(req.session.user._id)
-        .then((user) => {
-            if (!user) {
-                return next();
-            }
-            req.user = user;
-            next();
-        })
-        .catch((err) => {
-            next(new Error(err));
-        });
+      }
+      req.user = user;
+      next();
+    })
+    .catch((err) => {
+      next(new Error(err));
+    });
 });
 
 app.use("/admin", adminRoutes);
@@ -73,18 +73,18 @@ app.use("/500", errorController.get500);
 app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
-    res.status(500).render("500", {
-        pageTitle: "Error!",
-        path: "/500",
-        isAuthenticated: req.session.isLoggedIn,
-    });
+  res.status(500).render("500", {
+    pageTitle: "Error!",
+    path: "/500",
+    isAuthenticated: req.session.isLoggedIn,
+  });
 });
 
 mongoose
-    .connect(process.env.MONGO_URI)
-    .then((result) => {
-        app.listen(3000);
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+  .connect(process.env.MONGO_URI)
+  .then((result) => {
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
